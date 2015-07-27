6d4921b3d416df5791bed7c6993281b711483c4d
Ehsan <ehsan.silverman@gmail.com>
2015-07-27 08:57:36 +0430

    added testdiv

--- src/Test.java
+++ src/Test.java
@@ -21,4 +21,21 @@ class Test
             System.out.println("Sum: Failed, expected = " + expected + ", result = " + result);
         }
     }
+	
+    private static void testDiv()
+    {
+        int a = 30;
+        int b = 6;
+        int expected = 5;
+        BasicMath bm = new BasicMath();
+        int result = bm.divide(a, b);
+        if (result == expected)
+        {
+            System.out.println("Sum: OK");
+        }
+        else
+        {
+            System.out.println("Sum: Failed, expected = " + expected + ", result = " + result);
+        }
+    }
 }
